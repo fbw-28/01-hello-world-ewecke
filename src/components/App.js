5eda4eec2362aee68066cfbedde051b5623205fe
eweck <edgargandolfi@gmail.com>
2020-08-10 16:24:54 +0200

    react-ex1-edgar

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./App.scss"
+
+export default class App extends React.Component{
+
+    render(){
+        return (
+        <div>
+            <h1>Hello Word</h1>
+        </div>
+        )
+    }
+}
